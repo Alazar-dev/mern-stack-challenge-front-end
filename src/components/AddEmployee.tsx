30675497831d6bf1ed03698cc3a471da0242a355
Alazar-dev <mealazartadesse@gmail.com>
2020-11-17 14:01:45 +0300

    added dependency and edited component

--- src/components/AddEmployee.tsx
+++ src/components/AddEmployee.tsx
@@ -11,11 +11,18 @@ constructor(props: {}){
         name: '',
         date_of_birth: '',
         gender: '',
-        salary: ''
+        salary: '',
+        employee: []
        
     };
     this.handleSubmit = this.handleSubmit.bind(this)
 }
+
+    componentDidMount(){
+        fetch('/employee')
+        .then(res => res.json())
+        .then(users => this.setState({ employee }));
+    }
 
     handleNameChange = (e:any) =>{
         this.setState({
@@ -71,47 +78,55 @@ constructor(props: {}){
 
     render(){
         return (
-            <Form onSubmit = {this.handleSubmit}>
-                <div>
-                    <label>Full name</label>
-                    <input type="text"
-                        name="name"
-                        placeholder= "Your name"
-                        value = {this.state.name}
-                        onChange={this.handleNameChange} 
-                    />
-                </div>
-                <div>
-                    <label>Date of birth</label>
-                    <input type="date"
-                        name="date of birth"
-                        placeholder= "Your date of birth"
-                        value = { this.state.date_of_birth}
-                        onChange={this.handleDateChange}
-                    />
-                </div>
-                <div>
-                    <label>Gender</label>
-                    <input type="text"
-                        name="gender"
-                        placeholder= "Employee gender"
-                        value = {this.state.gender}
-                        onChange={this.handleGenderChange}
-                    />
-                </div>
-                <div>
-                    <label>Salary</label>
-                    <input type="number"
-                        name="salary"
-                        placeholder= "Employee salary"
-                        value = {this.state.salary}
-                        onChange={this.handleSalaryChange}
-                    />
-                </div>
+            <React.Fragment>
+                <h1>Employees</h1>
+                <ul>
+                    {this.state.employee.map(user => 
+                        <li key = {employee.id}>{user.name }</li>
+                    )}
+                </ul>
+                <Form onSubmit = {this.handleSubmit}>
+                    <div>
+                        <label>Full name</label>
+                        <input type="text"
+                            name="name"
+                            placeholder= "Your name"
+                            value = {this.state.name}
+                            onChange={this.handleNameChange} 
+                        />
+                    </div>
+                    <div>
+                        <label>Date of birth</label>
+                        <input type="date"
+                            name="date of birth"
+                            placeholder= "Your date of birth"
+                            value = { this.state.date_of_birth}
+                            onChange={this.handleDateChange}
+                        />
+                    </div>
+                    <div>
+                        <label>Gender</label>
+                        <input type="text"
+                            name="gender"
+                            placeholder= "Employee gender"
+                            value = {this.state.gender}
+                            onChange={this.handleGenderChange}
+                        />
+                    </div>
+                    <div>
+                        <label>Salary</label>
+                        <input type="number"
+                            name="salary"
+                            placeholder= "Employee salary"
+                            value = {this.state.salary}
+                            onChange={this.handleSalaryChange}
+                        />
+                    </div>
 
-                <Button type="submit">Register</Button>
+                    <Button type="submit">Register</Button>
 
-            </Form>
+                </Form>
+            </React.Fragment>
         )
            
     }
@@ -123,6 +138,7 @@ interface Istate{
     date_of_birth: string;
     gender: string;
     salary: string;
+    employee: array;
 }
 
 export default AddEmployee;
